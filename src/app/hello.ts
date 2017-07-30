7d28494ec3db4749ff83731a07cdb39dfd6543e9
Marcin Sirocki <marcinsirocki@gmail.com>
2017-07-30 15:02:38 +0200

    find contacts

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,28 +1,47 @@
 import {INgRedux} from 'ng-redux';
-import {fetchContacts} from '../redux/modules/contacts';
+import {fetchContacts, findContacts} from '../redux/modules/contacts';
 import {IOnDestroy, IOnInit} from 'angular';
+import {debounce} from 'lodash';
 
 class ContactsComponentController implements IOnInit, IOnDestroy {
   static $inject = ['$ngRedux'];
+  inputValue: string = '';
   unsubscribe: Function;
   fetchContacts: Function;
+  findContacts: Function;
+  findDebounce: Function;
 
   constructor(private $ngRedux: INgRedux) {
-    this.unsubscribe = $ngRedux.connect(state => ({contacts: state.contacts}), {fetchContacts})(this);
+    this.unsubscribe = $ngRedux.connect(state => ({contacts: state.contacts}), {fetchContacts, findContacts})(this);
   }
 
   $onInit(): void {
     this.fetchContacts();
+
+    this.findDebounce = debounce(() => {
+      this.findContacts(this.inputValue);
+    }, 500);
   }
 
   $onDestroy(): void {
     this.unsubscribe();
+  }
+
+  find(): void {
+    this.findDebounce();
   }
 }
 
 export const hello: angular.IComponentOptions = {
   template: `
     <md-content>
+      <md-toolbar>
+        <div layout="row" layout-align="center center">
+          <md-input-container md-no-float class="md-block">
+            <input type="text" placeholder="search contacts ..." style="color: #fff" ng-change="$ctrl.find()" ng-model="$ctrl.inputValue">
+          </md-input-container>
+        </div>
+      </md-toolbar>
       <md-list flex>
         <md-list-item immutable="$ctrl.contacts" class="md-3-line" ng-repeat="contact in $ctrl.contacts" ng-click="null">
           <img ng-src="{{contact.avatar}}" class="md-avatar" alt="{{concat.name}}" />
--- src/redux/modules/contacts.ts
+++ src/redux/modules/contacts.ts
@@ -4,6 +4,7 @@
  */
 import {data} from '../mock-data/data';
 import {List} from 'immutable';
+import {includes} from 'lodash';
 
 export type Contact = {
   name: string;
@@ -13,6 +14,7 @@ export type Contact = {
 };
 
 const LOAD_ALL_CONTACTS = 'LOAD_ALL_CONTACTS';
+const FIND_CONTACTS = 'FIND_CONTACTS';
 
 const initialState: List<Contact> = List([]);
 
@@ -27,11 +29,28 @@ export const fetchContacts = () => dispatch => {
   }, 1500);
 };
 
+export const findContacts = (term: string) => ({
+  type: FIND_CONTACTS,
+  payload: term
+});
+
 export default function reducer(state = initialState, action): List<Contact> {
   switch (action.type) {
     case LOAD_ALL_CONTACTS:
       return action.payload;
+    case FIND_CONTACTS:
+      return filterContacts(action.payload, state);
     default:
       return state;
   }
+}
+
+const isContainTerm = (term: string, contact: Contact): boolean =>
+includes(contact.name.toLocaleLowerCase(), term.toLocaleLowerCase()) ||
+includes(contact.email.toLocaleLowerCase(), term.toLocaleLowerCase()) ||
+includes(contact.phone, term.toLocaleLowerCase());
+
+
+function filterContacts(term: string, contacts: List<Contact>): List<Contact> {
+  return contacts.filter(contact => isContainTerm(term, contact)).toList();
 }
